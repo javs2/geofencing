03d2732250a306920c3ca13d273ef6ddd0e72331
javierHPA <javierp@integratto.com.mx>
2019-03-27 11:07:34 -0600

    only support for Android

---
+++ example/android/app/src/main/java/mx/com/integratto/plugin/geofence/geofencing_example/MainActivity.java
@@ -0,0 +1,13 @@
+package mx.com.integratto.plugin.geofence.geofencing_example;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
